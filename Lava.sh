de510175def0801864847759bc37a1391a09161b
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-03-31 02:05:26 +0300

    Create Lava.sh

---
+++ Lava.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Dikci/Pears/blob/main/peerskjnodes.js
+wget https://github.com/Dikci/Pears/blob/main/peersnodejumper.js
+wget https://raw.githubusercontent.com/Dikci/Pears/main/Start.sh && chmod +x Start.sh && ./Start.sh
